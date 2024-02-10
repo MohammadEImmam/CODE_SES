00c010e1cfa7d13113ae93fa3b46e8c03d13f8b2
MohammadEImmam <93059810+MohammadEImmam@users.noreply.github.com>
2024-02-10 11:40:25 -0500

    Test Case Stuff

---
+++ Assets/Scripts/CrossConnections/CaseMatcher.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using RoslynCSharp;
+
+namespace CrossConnections
+{
+    public class CaseMatcher
+    {
+        //Plugin controller to interact with user code
+        private ScriptType UserCode;
+        //Name of the method from user code to run and check value of
+        private string methodName;
+        //List of test cases to compare with user code
+        List<TestCase> testCases;
+        
+        public CaseMatcher(ScriptType UserCode, string methodName, List<TestCase> testCases)
+        {
+            this.UserCode = UserCode;
+            this.methodName = methodName;
+            this.testCases = testCases;
+        }
+
+
+        public void MatchCases()
+        {
+            foreach (TestCase testCase in testCases)
+            {
+                
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/CrossConnections/TestCase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Unity.VisualScripting;
+
+namespace CrossConnections
+{
+    public class TestCase
+    {
+        //optional identifying params
+        private string name;
+        private string description;
+        private string expectedOutput;
+        private List<string> parameters;
+
+        TestCase(string name, string description, string expectedOutput, List<string> parameters)
+        {
+            this.name = name;
+            this.description = description;
+            this.expectedOutput = expectedOutput;
+            this.parameters = parameters;
+        }
+
+        TestCase(string expectedOutput, List<string> parameters)
+        {
+            this.expectedOutput = expectedOutput;
+            this.parameters = parameters;
+        }
+    }
+}
